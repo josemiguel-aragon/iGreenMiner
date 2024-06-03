53b43928f213746b035daf636e10273afc615c68
maple <maple@maples-MacBook-Air.local>
2024-06-03 11:01:45 -0600

    CSV Sync

--- monsoon-0.1.88/MonsoonServerSync/main.py
+++ monsoon-0.1.88/MonsoonServerSync/main.py
@@ -4,11 +4,14 @@ import Monsoon.Operations as op
 import Monsoon.sampleEngine as sampleEngine
 
 import json
+import os
 import sys
 import time
+import pandas as pd
 
 from pathos.multiprocessing import ProcessingPool as Pool
 
+sample_rate = 5000
 start_measurement = False
 begin_time = 0
 end_time = 0
@@ -20,7 +23,7 @@ Mon.setup_usb()
 Mon.setUSBPassthroughMode(op.USB_Passthrough.On)
 HVengine = sampleEngine.SampleEngine(Mon)
 
-#HVengine.enableCSVOutput("./test.csv")
+HVengine.enableCSVOutput("./measurement.csv")
 HVengine.ConsoleOutput(False)
 
 #Setting trigger conditions
@@ -51,25 +54,39 @@ def startMeasurement():
 
 @get('/endMeasurement')
 def endMeasurement():
-    global start_measurement, end_time, elapsed_time, begin_time
+    global start_measurement, end_time, elapsed_time, begin_time, sample_rate
+    
     while start_measurement == False:
         pass
     ## End Monsoon sampling
     end_time = time.time()
     elapsed_time = end_time - begin_time
-    samples = HVengine.periodicCollectSamples(int(5000 * elapsed_time))
-    print(len(samples[sampleEngine.channels.timeStamp]))
+    samples = HVengine.periodicCollectSamples(int(sample_rate * elapsed_time))
+    #print(len(samples[sampleEngine.channels.timeStamp]))
     
     #Samples are stored in order, indexed sampleEngine.channels values
     #for i in range(len(samples[sampleEngine.channels.timeStamp])):
     #    timeStamp = samples[sampleEngine.channels.timeStamp][i]
     #    Current = samples[sampleEngine.channels.MainCurrent][i]
     #    print("Main current at time " + repr(timeStamp) + " is: " + repr(Current) + "mA")
-    print(samples[sampleEngine.channels.timeStamp][-1] - samples[sampleEngine.channels.timeStamp][0])
+    df = pd.read_csv("./measurement.csv", delimiter = ',')
+    print(df)
+    HVengine.periodicStopSampling()
+    os.remove("./measurement.csv")
+    HVengine.enableCSVOutput("./measurement.csv")
+    HVengine.enableChannel(sampleEngine.channels.MainCurrent)
+    HVengine.enableChannel(sampleEngine.channels.MainVoltage)
+    HVengine.enableChannel(sampleEngine.channels.timeStamp)
+    HVengine.periodicStartSampling()
+    times = df['Time(ms)']
+    
+    
+    print(times.iloc[-1] - times.iloc[0])
+    
     start_measurement = False
     
     with open("./accuracy_test.csv", "a+") as file:
-        file.write(f"{samples[sampleEngine.channels.timeStamp][-1] - samples[sampleEngine.channels.timeStamp][0]},")
+        file.write(f"{times.iloc[-1] - times.iloc[0]},")
     
     print("Ending measurement...")
     return {"REQUEST": "GOOD"}
--- monsoon-0.1.88/MonsoonServerSync/test_server.py
+++ monsoon-0.1.88/MonsoonServerSync/test_server.py
@@ -3,12 +3,12 @@ import requests, time
 config_ip = "192.168.1.101"
 
 if __name__ == '__main__':
-    for i in range(1):
+    for i in range(2):
         
         requests.get(f"http://{config_ip}:8089/startMeasurement")
         
         # APP EXECUTION TIME
-        time.sleep(10)
+        time.sleep(2)
         
         requests.get((f"http://{config_ip}:8089/endMeasurement"))
     
